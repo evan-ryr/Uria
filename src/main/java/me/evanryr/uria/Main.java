22c54df87cb88810737c52d970c0f266fb611909
evan-ryr <evanroyer1008@icloud.com>
2024-03-06 20:01:54 +0100

    First Commit: Création du plugin

---
+++ src/main/java/me/evanryr/uria/Main.java
@@ -0,0 +1,11 @@
+package me.evanryr.uria;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Bonjour !");
+    }
+}
